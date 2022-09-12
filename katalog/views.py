6cc105b784f1f98b9a279169d6136e5fd5c0dd44
azmirahmadisha <azmi.rahmadisha@ui.ac.id>
2022-09-13 01:48:50 +0700

    Succesfully showed data

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+    'list_item': data_barang_katalog,
+    'nama': 'Adish',
+    'studentID': '2106751285'
+    }
+    return render(request, "katalog.html", context)
