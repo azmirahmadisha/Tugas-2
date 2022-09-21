9a48d57109831986be7354d9a15e2e4c7cf0fa22
azmirahmadisha <azmi.rahmadisha@ui.ac.id>
2022-09-21 21:04:55 +0700

    Updated tests.py

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import resolve
 
 # Create your tests here.
+
+class TestingWatchlist(TestCase):
+    def testing_app_url(self):
+        response = self.client.get('/mywatchlist/')
+        self.assertEqual(response.status_code,200)
+
+    def testing_app_url_json(self):
+        response = self.client.get('/mywatchlist/json/')
+        self.assertEqual(response.status_code,200)
+
+    def testing_app_url_xml(self):
+        response = self.client.get('/mywatchlist/xml/')
+        self.assertEqual(response.status_code,200)
+
+    def testing_app_url_html(self):
+        response = self.client.get('/mywatchlist/html/')
+        self.assertEqual(response.status_code,200)
+
+    
